7dd7dc81f5b938dd9f41c4c6ccc9036933381535
xiaojinlu <xiaojinlu1990@Gmail.com>
2021-02-04 23:49:23 +0800

    inearLayout(线性布局)

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,28 +1,22 @@
 package com.example.administrator.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.administrator.myapplication.utils.ShareUtils;
+import com.example.administrator.myapplication.ui.UiLinearLayout;
+
+/**
+ * 布局总控代码
+ */
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //定义xml属性
-    //注册按钮
-    private Button btn_registered;
-    private EditText et_name;
-    private EditText et_password;
-    private Button btnLogin;
-    private CheckBox keep_password;
+    //UiLinearLayout(线性布局)
+    private Button activityLinearLayou;
 
-    private TextView tv_forget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,47 +26,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-
-        btn_registered = (Button) findViewById(R.id.btn_registered);
-        btn_registered.setOnClickListener(this);
-        et_name = (EditText) findViewById(R.id.et_name);
-        et_password = (EditText) findViewById(R.id.et_password);
-        btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(this);
-        keep_password = (CheckBox) findViewById(R.id.keep_password);
-        tv_forget = (TextView) findViewById(R.id.tv_forget);
-        tv_forget.setOnClickListener(this);
-
-        boolean isKeep = ShareUtils.getBoolean(this, "keeppass", false);
-        keep_password.setChecked(isKeep);
-
-
-
-        if(isKeep){
-            String name = ShareUtils.getString(this, "name", "");
-            String password = ShareUtils.getString(this, "password", "");
-            et_name.setText(name);
-            et_password.setText(password);
-        }
+        activityLinearLayou = (Button) findViewById(R.id.btn_linear_layout);
+        activityLinearLayou.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
-        System.out.println("btn_registered");
+        System.out.println("btn_linear_layout");
         switch (v.getId()) {
-            case R.id.btn_registered:
-                //1.获取输入框的值
-                String name = et_name.getText().toString().trim();
-                String password = et_password.getText().toString().trim();
-                //2.判断是否为空
-                if (!TextUtils.isEmpty(name) & !TextUtils.isEmpty(password)) {
-                    startActivity(new Intent(this, PolicyActivity.class));
-                }else {
-                    Toast.makeText(this, "用户名密码不能为空!!!", Toast.LENGTH_SHORT).show();
-                }
+            case R.id.btn_linear_layout:
 
-                break;
-            case R.id.btnLogin:
-                Toast.makeText(this, "请先注册!!!", Toast.LENGTH_SHORT).show();
+                System.out.println("btn_linear_layout");
+                startActivity(new Intent(this, UiLinearLayout.class));
                 break;
         }
     }
@@ -82,3 +46,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 }
+
