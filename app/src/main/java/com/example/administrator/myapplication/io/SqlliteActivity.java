ba8842539450c194c61d5f4358d0bf0e3bebb17a
xiaojinlu <xiaojinlu1990@Gmail.com>
2021-02-25 20:44:16 +0800

    添加sqllit查询

--- app/src/main/java/com/example/administrator/myapplication/io/SqlliteActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/io/SqlliteActivity.java
@@ -1,62 +1,126 @@
 package com.example.administrator.myapplication.io;
 
-import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import com.example.administrator.myapplication.R;
+import com.example.administrator.myapplication.dao.StudentDao;
+import com.example.administrator.myapplication.entity.Student;
 
 
 public class SqlliteActivity extends AppCompatActivity {
 
-    private EditText accEdt,pwdEdt;
+    private EditText nameEdt , ageEdt , idEdt;
+    private RadioGroup genderGp;
+    private ListView stuList;
+    private RadioButton malerb;
+    private String genderStr = "男";
+    private StudentDao dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slqlite);
 
-        accEdt = findViewById(R.id.acc_edt);
-        pwdEdt = findViewById(R.id.pwd_edt);
+        dao = new StudentDao(this);
 
-        //SharePreference的读取
-        //①获取SharePreference对象(参数1：文件名  参数2：模式)
-        SharedPreferences share = getSharedPreferences("myshare",MODE_PRIVATE);
-        //②根据key获取内容(参数1：key   参数2：当对应key不存在时，返回参数2的内容作为默认值)
-        String accStr = share.getString("account","");
-        String pwdStr = share.getString("pwd","");
+        nameEdt = (EditText) findViewById(R.id.name_edt);
+        ageEdt = (EditText) findViewById(R.id.age_edt);
+        idEdt = (EditText) findViewById(R.id.id_edt);
+        malerb = (RadioButton) findViewById(R.id.male);
 
-        accEdt.setText(accStr);
-        pwdEdt.setText(pwdStr);
-
-        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
+        genderGp = (RadioGroup) findViewById(R.id.gender_gp);
+        genderGp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                //1.获取两个输入框的内容
-                String account = accEdt.getText().toString();
-                String pwd = pwdEdt.getText().toString();
-                //2.验证(admin  123)
-                if(account.equals("admin") && pwd.equals("123456")){
-                    //2.1存储信息到SharePreference
-                    //①获取SharePreference对象(参数1：文件名  参数2：模式)
-                    SharedPreferences share = getSharedPreferences("myshare",MODE_PRIVATE);
-                    //②获取Editor对象
-                    SharedPreferences.Editor edt = share.edit();
-                    //③存储信息
-                    edt.putString("account",account);
-                    edt.putString("pwd",pwd);
-                    //④指定提交操作
-                    edt.commit();
-
-                    Toast.makeText(SqlliteActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
-                }else {
-                    //2.2验证失败，提示用户
-                    Toast.makeText(SqlliteActivity.this,"账号或密码错误",Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == R.id.male){
+                    //“男”
+                    genderStr = "男";
+                }else{
+                    //"女"
+                    genderStr = "女";
                 }
             }
         });
+
+        stuList = (ListView) findViewById(R.id.stu_list);
+    }
+
+    public void operate(View v){
+
+        String nameStr = nameEdt.getText().toString();
+        String ageStr = ageEdt.getText().toString();
+        String idStr = idEdt.getText().toString();
+        switch (v.getId()){
+            case R.id.insert_btn:
+                Student stu = new Student(nameStr,Integer.parseInt(ageStr),genderStr);
+                dao.addStudent(stu);
+                Toast.makeText(this,"添加成功",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.select_btn:
+                String key="",value="";
+                if(!nameStr.equals("")){
+                    value = nameStr;
+                    key = "name";
+                }else if(!ageStr.equals("")){
+                    value = ageStr;
+                    key = "age";
+                }else if(!idStr.equals("")){
+                    value = idStr;
+                    key = "_id";
+                }
+                Cursor c;
+                if(key.equals("")){
+                    c = dao.getStudent();
+                }else {
+                    c = dao.getStudent(key, value);
+                }
+
+                SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                        this, R.layout.item,c,
+                        new String[]{"_id","name","age","gender"},
+                        new int[]{R.id.id_item,R.id.name_item,R.id.age_item,R.id.gender_item});
+                stuList.setAdapter(adapter); /**/
+                break;
+            case R.id.delete_btn:
+
+                String[] params = getParams(nameStr,ageStr,idStr);
+
+                dao.deleteStudent(params[0],params[1]);
+                Toast.makeText(this,"删除成功",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.update_btn:
+                Student stu2 = new Student(Integer.parseInt(idStr),nameStr,Integer.parseInt(ageStr),genderStr);
+                dao.updateStudent(stu2);
+                Toast.makeText(this,"修改成功",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        nameEdt.setText("");
+        ageEdt.setText("");
+        idEdt.setText("");
+        malerb.setChecked(true);
+    }
+
+    public String[] getParams(String nameStr,String ageStr,String idStr){
+        String[] params = new String[2];
+        if(!nameStr.equals("")){
+            params[1] = nameStr;
+            params[0] = "name";
+        }else if(!ageStr.equals("")){
+            params[1] = ageStr;
+            params[0] = "age";
+        }else if(!idStr.equals("")){
+            params[1] = idStr;
+            params[0] = "_id";
+        }
+        return params;
     }
 
 }
