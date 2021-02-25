89821dd6082422b1f2fcca20edc7969b96395caf
xiaojinlu <xiaojinlu1990@163.com>
2021-02-25 16:56:53 +0800

    添加sqllite框架

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.administrator.myapplication.io.SharedpreFerenceActivity;
+import com.example.administrator.myapplication.io.SqlliteActivity;
 import com.example.administrator.myapplication.utils.L;
 
 /**
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //定义xml属性
     //activityHandlerOne(handler的基本处理)
     private Button activityHandlerOne;
+
+    //sqllite的是数据实例
+    private Button textSqlLite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         activityHandlerOne.setOnClickListener(this);
 
 
+        textSqlLite = (Button) findViewById(R.id.btn_sqllite);
+        //这里必须添加应用事件
+        textSqlLite.setOnClickListener(this);
+
+
     }
     @Override
     public void onClick(View v) {
@@ -39,6 +48,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_io_one:
             L.i("btn_handler_one");
             startActivity(new Intent(this, SharedpreFerenceActivity.class));
+            case R.id.btn_sqllite:
+            L.i("btn_sqllite");
+            startActivity(new Intent(this, SqlliteActivity.class));
             break;
 
         }
